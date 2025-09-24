e3e61bbf7ac1881a1774a7c345362d09009ff53c
TheOfficialFathyAdel <crowleys090@gmail.com>
2025-09-24 16:26:15 +0300

    Delete app.tsx and index.ts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={tw`font-bold text-4xl`}>Hello world again</Text>
+    </SafeAreaView>
   );
 }
